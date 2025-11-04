53846645f532139362114467726f27c17a9f5fff
Zabuka_zuzu <zuzu950444@gmail.com>
2025-11-04 06:35:09 +0000

    vendor: added Progress & Notification Themes

--- config.mk
+++ config.mk
@@ -302,6 +302,21 @@ PRODUCT_PACKAGES += \
     PowerIOS \
     PowerLayers
 
+# Progress Bar Themes
+PRODUCT_PACKAGES += \
+    PGB_BlockyThumb \
+    PGB_MinimalThumb \
+    PGB_OutlineThumb \
+    PGB_Shishu
+
+# Notification Themes
+PRODUCT_PACKAGES += \
+    NotifCyberPunk \
+    NotifDuoline \
+    NotifFluid \
+    NotifIOS \
+    NotifLayers
+
 # Signal Icons
 PRODUCT_PACKAGES += \
     AquariumSignalOverlay \
